4a57bd84b7ef99967d9aff7814778f8597dbebbb
Tianyong Tang <tang3w@gmail.com>
2015-01-26 10:54:13 +0800

    Fix the missing header

--- COSStyle/COSStyleNodes/COSStyleNode.h
+++ COSStyle/COSStyleNodes/COSStyleNode.h
@@ -24,6 +24,7 @@
 // OTHER DEALINGS IN THE SOFTWARE.
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 #import "COSStyleDefine.h"
 
--- COSStyle/COSStyleNodes/COSStyleNodeProp.h
+++ COSStyle/COSStyleNodes/COSStyleNodeProp.h
@@ -23,8 +23,6 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 
-#import <UIKit/UIKit.h>
-
 #import "COSStyleNode.h"
 #import "COSStyleNodeVal.h"
 
